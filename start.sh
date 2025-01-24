9a4649cd900b1280daf1123728bb2a999146f30e
Bisnu Ray <79582318+bisnuray@users.noreply.github.com>
2025-01-24 14:02:42 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/bisnuray/TelecastBot /TelecastBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/bisnuray/TelecastBot -b $BRANCH /TelecastBot
 fi
-cd /VCPlayerBot
+cd /TelecastBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
